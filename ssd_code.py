fe409f544b75e230b1725eee596b894a9c2d2191
Taha Naveed <tahanaveed@hotmail.com>
2022-04-22 15:36:50 +0500

    changed code

--- ssd_code.py
+++ ssd_code.py
@@ -1,20 +1,11 @@
 import numpy as np
 import time
 import cv2
-from tkinter import filedialog
-from tkinter import Tk
-import matplotlib.pyplot as plt
-import random
-import os
-
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 "sofa", "train", "tvmonitor"]
-print('Total Number of classes are: {} \n'.format(len(CLASSES)-1))
-np.random.seed(50)
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES),3))
 weights ='MobileNetSSD_deploy.caffemodel'
 
 # Load the Model
@@ -24,27 +15,29 @@ net = cv2.dnn.readNetFromCaffe(arch, weights)
 
 ###########################################################################################
 
-threshold = 0.3
+threshold = 0.6
 cap=cv2.VideoCapture(0)
 w  = cap.get(3)
 h = cap.get(4)
 
+
 while(True):
-    start_time=time.time()
+
+    t1=time.time()
     ret,frame=cap.read()
-    blob = cv2.dnn.blobFromImage(frame,  0.007843,(128, 128), (127.5,127.5,127.5),True)
+    blob = cv2.dnn.blobFromImage(frame,  0.007843,(320,320), (127.5,127.5,127.5),True)
 
     net.setInput(blob)
     detections = net.forward()
 
     for i in np.arange(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
+        # print(detections)
 
         # Filter Out weak detections
         if confidence > threshold:
             # Extracting the index of the class.
             index = int(detections[0, 0, i, 1])
-
             # Extract the (x1,x2,y1,y2) coordinates of the bounding box for each obejct 
             x_1 = int(detections[0, 0, i, 3] * w)
             y_1 = int(detections[0, 0, i, 4] * h)
@@ -52,19 +45,18 @@ while(True):
             y_2 = int(detections[0, 0, i, 6] * h)
 
             if index==15:
-                # Display the target class name and their confidence score.
                 label = "{}: {:.2f}%".format(CLASSES[index], confidence * 100)
-                # Draw the rectangle on object w.r.t their cordinates. 
-                cv2.rectangle(frame, (x_1, y_1), (x_2, y_2), COLORS[index], 4)
+                cv2.rectangle(frame, (x_1, y_1), (x_2, y_2), (0,0,255), 4)
+                cv2.putText(frame, label, (x_1, y_1), cv2.FONT_ITALIC, 2, (0,0,255), 4)
+                print('person found')
+                t2=time.time()
+                # print('Time: ',t2-t1)
+            else:
+                print('Person Not Found')
+                pass
 
-                # Put the Label of the detected object 
-                cv2.putText(frame, label, (x_1, y_1), cv2.FONT_ITALIC, 1.3, COLORS[index], 3)
 
-
-    fps= (1.0 / (time.time() - start_time))
-    cv2.putText(frame, 'FPS: {:.2f}'.format(fps), (400, 20), cv2.FONT_HERSHEY_SIMPLEX,0.8, (255, 20, 55), 2)
     cv2.imshow("Image",frame)
-    
     k = cv2.waitKey(1)   
     if k == ord('q'):
         break
